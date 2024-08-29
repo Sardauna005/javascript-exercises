369d13b3cfbde6b817494bee9ae87499a207c6c3
Ahmed Galadima <ahmedgaladima05@gmail.com>
2024-08-29 21:34:57 +0100

    DID: FINISH EXERCISE ONE ET TWO

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function (word, rep) {
+  let answer = [];
+  if (rep >= 0) {
+    for (let i = 0; i < rep; i++) {
+      answer.push(word);
+    }
+    answer = answer.join("");
+    return answer;
+  } else {
+    answer = "ERROR";
+    return answer;
+  }
 };
-
 // Do not edit below this line
 module.exports = repeatString;
