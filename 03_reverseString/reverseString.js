b64696bae0bfed18e63d7b97c8dfc6977e08f9e1
Ahmed Galadima <ahmedgaladima05@gmail.com>
2024-08-30 20:22:33 +0100

    DID: 2 MORE EXERCISES: 8 LEFT

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function (word) {
+  let stringHolder = [];
+  for (i = word.length - 1; i >= 0; i--) {
+    stringHolder.push(word[i]);
+  }
+  let reversedString = stringHolder.join("");
+  return reversedString;
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+// here is a one liner version of this code not that i like it but it works far better than mine
+// return string.split("").reverse().join("");
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+  // a function that removes the argument provided from a given array.
+  //   return (answerArray = arr.filter((val) => !args.includes(val)));
+  let answerArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (!args.includes(arr[i])) {
+      answerArray.push(arr[i]);
+    }
+  }
+  return answerArray;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
