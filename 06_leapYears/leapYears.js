18fc6accc08e91a3f2bd3c8c14be0f5b16baec94
Ahmed Galadima <ahmedgaladima05@gmail.com>
2024-08-31 15:07:31 +0100

    DID: FINISHED ALL 7 ASSIGNMENTS

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  //   if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  //   a simpler clearer version that evaluates directly.
+  return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
 };
-
 // Do not edit below this line
 module.exports = leapYears;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  // converting fahrenheit to celsius -> formula is (x - 32 * 5/9)
+  fahrenheit = ((fahrenheit - 32) * 5) / 9;
+  return Math.round(fahrenheit * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  // converting celsius to fahrenheit -> formula is (x * 9/5 + 32)
+  celsius = (celsius * 9) / 5 + 32;
+  return Math.round(celsius * 10) / 10;
 };
+
+console.log(convertToCelsius(100));
+console.log(convertToFahrenheit(37.8));
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
